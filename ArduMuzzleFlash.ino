df268f389ef00494d0303cfe6b51f4ccc9815b10
vogt31337 <vogt31337@gmx.de>
2017-08-29 17:01:35 +0200

    removed unused comments. Added a .hex version for directly programming.

--- ArduMuzzleFlash.ino
+++ ArduMuzzleFlash.ino
@@ -6,26 +6,6 @@
 #define LED_TYPE    NEOPIXEL
 #define COLOR_ORDER RGB
 CRGB leds[NUM_LEDS];
-
-#define UPDATES_PER_SECOND 100
-
-// This example shows several ways to set up and use 'palettes' of colors
-// with FastLED.
-//
-// These compact palettes provide an easy way to re-colorize your
-// animation on the fly, quickly, easily, and with low overhead.
-//
-// USING palettes is MUCH simpler in practice than in theory, so first just
-// run this sketch, and watch the pretty lights as you then read through
-// the code.  Although this sketch has eight (or more) different color schemes,
-// the entire sketch compiles down to about 6.5K on AVR.
-//
-// FastLED provides a few pre-configured color palettes, and makes it
-// extremely easy to make up your own color schemes with palettes.
-//
-// Some notes on the more abstract 'theory and practice' of
-// FastLED compact palettes are at the bottom of this file.
-
 CRGB light = CRGB(200, 100, 20);
 int oldState = 1;
 int cylonPos = 0;
